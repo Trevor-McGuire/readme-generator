105390abeb38b075cbfa625469a079e8f0f87683
Trevor-McGuire <trevor.mcguire.candidate@gmail.com>
2023-04-29 20:25:38 -0500

    template.md to template literal with values

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-const inquirer = require('inquirer');
+const inquirer = require('inquirer')
+const fs = require('fs')
 
 inquirer
   .prompt([
     {
       type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
-    },
-    {
-      type: 'password',
-      message: 'What is your password?',
-      name: 'password',
-    },
-    {
-      type: 'password',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
+      message: 'What is the title of the project?',
+      name: 'title',
     },
   ])
   .then((response) =>
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('You forgot your password already?!')
-  );
+    fs.readFile('template.md', 'utf8', function(error, data) {
+      if (error) {
+        console.error(error)
+      } else {
+        
+      }
+      String.prototype.interpolate = function(params) {
+        const names = Object.keys(params);
+        const vals = Object.values(params);
+        return new Function(...names, `return \`${this}\`;`)(...vals);
+      }
+      
+      const template = data;
+      const result = template.interpolate({
+        title: response.title
+      });
+      console.log(result);
+    })
+    // fs.appendFile('README.md', process.argv[2], (err) =>
+    //   err ? console.error(err) : console.log('Success!')
+    // )
+  )
